8651c70a458556d31dabf3b9220c2157a5ebe9b6
xTarantino28 <gzanabriaz28@gmail.com>
2023-09-17 15:44:54 -0500

    vibracion cuando termina el counter

--- app/src/main/java/com/example/lab2_20200334_iot/CounterActivity.java
+++ app/src/main/java/com/example/lab2_20200334_iot/CounterActivity.java
@@ -1,5 +1,7 @@
 package com.example.lab2_20200334_iot;
-
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -13,10 +15,12 @@ import com.example.lab2_20200334_iot.threads.AppThreads;
 import com.example.lab2_20200334_iot.viewModels.CounterViewModel;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class CounterActivity extends AppCompatActivity {
     private ActivityCounterBinding binding;
     private int increment = 10;
+     int i = 104;
     private boolean firstClick = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +37,12 @@ public class CounterActivity extends AppCompatActivity {
             binding.counterTextView.setText(String.valueOf(counter));
         });
 
+
+
         binding.Start.setOnClickListener(view -> {
             if ( firstClick ) {
                 executorService.execute(() -> {
-                    for (int i = 104; i <= 226; i = i+increment) {
+                    for (i = 104; i <= 226; i = i+increment) {
                         counterViewModel.getCounter().postValue(i);
                         Log.d("msg-test", "i: " + i);
                         try {
@@ -44,21 +50,34 @@ public class CounterActivity extends AppCompatActivity {
                         } catch (InterruptedException e) {
                             throw new RuntimeException(e);
                         }
+                        if (  (226-(i+increment) <= 0)) {
+                            counterViewModel.getCounter().postValue(226);
+                            Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                            // Vibrate for 500 milliseconds
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                            } else {
+                                //deprecated in API 26
+                                v.vibrate(500);
+                            }
+                        }
                     }
                 });
                 firstClick = false;
+
             } else {
                 increment = (int) (increment + Math.round(0.5*increment));
             }
 
         });
 
-
         binding.Reset.setOnClickListener(view -> {
             counterViewModel.getCounter().postValue(104);
             firstClick = true;
             increment = 10;
+            i = 104;
         });
+
 
     }
 }
