1ddba89b325076c5419222fadb4e38e3bb77022d
xTarantino28 <gzanabriaz28@gmail.com>
2023-09-17 04:38:57 -0500

    disenio planteado. pagina de inicio terminada. campos y checkbox de create account validados. falta menu con consulta a API, cronometro y contador

--- app/src/main/java/com/example/lab2_20200334_iot/MainActivity.java
+++ app/src/main/java/com/example/lab2_20200334_iot/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.lab2_20200334_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab2_20200334_iot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lab2_20200334_iot/SignUpActivity.java
@@ -0,0 +1,46 @@
+package com.example.lab2_20200334_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.lab2_20200334_iot.databinding.ActivityMainBinding;
+import com.example.lab2_20200334_iot.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText nameEditText =  binding.nameTextField.getEditText();
+                EditText lastNameEditText =  binding.lastNameTextField.getEditText();
+                EditText emailEditText =  binding.emailTextField.getEditText();
+                EditText passwordEditText =  binding.passwordTextField.getEditText();
+                boolean checkBoxBoolean = binding.checkBox.isChecked();
+                boolean allFieldsFilled = (nameEditText!=null) && (lastNameEditText!=null) && (emailEditText!=null) && (passwordEditText!=null) && checkBoxBoolean;
+                if (allFieldsFilled) {
+                    Intent intent = new Intent(SignUpActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Complete todos los campos para continuar", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+}
